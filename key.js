d74c6267e95f996e6aa29040dd810d7cd580c832
rhssr <38734752+rhssr@users.noreply.github.com>
2018-04-25 17:03:18 -0400

    Add files via upload
    I created a error 404 website, is currently working 24/7 and was builded with a stylesheet (css), <meta> tag and JavaScript, the page(html) has comments and secret references that would give me pleasure that someone with free time to discover, they are in the source code of the page (html) and they are not very difficult to find, how to do it: 1. Right click on any part of the page.
    2. Give the option: See source code
    3. (optional) To save time and not have to be giving the right click can enter the page / html and press Ctrl + U and it will open automatically, there are also more secrets but I want you to discover them, hahaha!

---
+++ key.js
@@ -0,0 +1,19 @@
+document.onkeydown = checkKeycode
+function checkValue(405)
+function checkKeycode(e) 
+{
+    if (input.text) value = 405;
+    var value;
+    if (value == 405)
+    href(opacity-deletedHTML/falseerror.html);
+    }
+{
+    var keycode;
+    if (window.event) keycode = window.event.keyCode;
+    else if (e) keycode = e.which;
+    // alert("keycode: " + keycode);
+    if (keycode == 13)
+    {
+        href("errorcode.html");
+    }
+}
